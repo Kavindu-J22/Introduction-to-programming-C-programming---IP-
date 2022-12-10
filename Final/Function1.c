cc6da329c6d7391dee8f049d21a5fc5936b17b1e
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-10 21:36:21 +0530

    upload

---
+++ Final/Function1.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int square ( int y ) ;
+int main ( void )
+{
+int x;
+for (x =1; x <= 10; ++x){
+	printf("%d", square( x ));
+	puts(" ");	
+}
+
+}
+
+
+int square (int y)
+{
+return y * y;
+}
+
