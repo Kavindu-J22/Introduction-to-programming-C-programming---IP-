4a547d592daa1bccc0ed570350b89b36d98581ff
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-13 01:36:07 +0530

    upload

--- Final/2018Q2.c
+++ Final/2018Q2.c
@@ -9,20 +9,33 @@ struct bank{
 
 int main (void){
 	int i,j;
+	double totdep=0,totwith=0;
+	char maxdname[10];
+	char maxwithname[10];
+	
 	struct bank cus[5][4];
 	
 	for(i=0; i<5; ++i){
-			printf("Enter Account Number : ");
-			scanf(" %d",&cus[i][0].accno);
-			printf("Enter Customer name : ");
-			scanf(" %s",&cus[i][1].name);
-			printf("Enter Customer Transaction Type : ");
-			scanf(" %c", &cus[i][2].TransType);
-			printf("Enter Amount : ");
-			scanf(" %lf",&cus[i][3].amount);
-			printf("\n");
+		printf("Enter Account Number : ");
+		scanf(" %d",&cus[i][0].accno);
+		printf("Enter Customer name : ");
+		scanf(" %s",&cus[i][1].name);
+		printf("Enter Customer Transaction Type(W/T) : ");
+		scanf(" %c", &cus[i][2].TransType);
+		printf("Enter Amount : ");
+		scanf(" %lf",&cus[i][3].amount);
+		printf("\n");
 	}
 	
+	for(i=0; i<5; ++i){
+		if(cus[i][2].TransType=='W')
+			totwith+=cus[i][3].amount;
+		else
+			totdep+=cus[i][3].amount;		
+	}
+	
+	printf("Total deposit amount : %.2f",totdep);
+	printf("Total Withdrow amount : %.2f",totwith);
 	
 	return 0;
 }
